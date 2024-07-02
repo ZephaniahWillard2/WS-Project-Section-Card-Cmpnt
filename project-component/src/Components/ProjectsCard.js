ffd9fc160032430ae041728f76d1416c053fde94
Zephaniah-Willard <zepiwillard@gmail.com>
2024-07-02 16:58:49 -0500

    Finished annotating and cleaning code up a bit, ready to be looked over

--- project-component/src/Components/ProjectsCard.js
+++ project-component/src/Components/ProjectsCard.js
@@ -2,25 +2,32 @@ import React, { useState } from "react";
 import "../App.css";
 
 const ProjectsCard = () => {
-  const [isTextareaFocused, setIsTextareaFocused] = useState(false);
-  const [title, setTitle] = useState("Full Wedding Package");
-  const [description, setDescription] = useState(
+  const [isTextareaFocused, setIsTextareaFocused] = useState(false);    //Created a state variable and a function to update the variable's state from "Not Focused" to "Focused".
+  const [title, setTitle] = useState("Full Wedding Package");                   //Not important
+  const [description, setDescription] = useState(                               //Not important
     "Includes a complete and mastered wedding album with up to 10 songs and recordings."
   );
-  const [price, setPrice] = useState("5000");
+  const [price, setPrice] = useState("5000");                                   //Not important
 
-  const handleFocus = () => {
-    console.log("Focused");
+  const handleFocus = () => {                                           //Made a function to handle the onFocus event and change the state of the state variable,
+    console.log("Focused");                                             //using the setIsTextareaFocused function, from "false" to "true".
     setIsTextareaFocused(true);
   };
 
-  const handleBlur = () => {
-    console.log("Blurred");
+  const handleBlur = () => {                                            //Made a function to change the state of the state variable, using the setIsTextareaFocused function,
+    console.log("Blurred");                                             //back to "false" when the textarea is no longer onFocus.
     setIsTextareaFocused(false);
   };
 
   return (
-    <div className="card" style={{ display: "flex", paddingLeft: "30%", paddingTop: "10%" }}>
+    <div className="card"
+        style={{
+          display: "flex",
+          paddingLeft: "30%",
+          paddingTop: "10%"
+        }}>
+{/* Changed some of the styling in various sections not for functional purposes, just for stylistic purposes. */}
+{/* None of these specific changes are meant to relfect a change in the original component. */}
       <section
         style={{
           padding: "0px",
@@ -33,10 +40,14 @@ const ProjectsCard = () => {
           background: "rgb(255, 255, 255)",
           boxShadow: "rgba(0, 0, 0, 0.1) 1px 2px 3px",
           display: "block",
-          width: "90%",
-        }}
-      >
-        <div className="flex-c" style={{ alignItems: "stretch", display: "flex", alignItems: "center" }}>
+          width: "70%",
+        }}>
+        <div className="flex-c"
+            style={{
+              alignItems: "stretch",
+              display: "flex",
+              alignItems: "center"
+            }}>
           <div
             style={{
               flex: "1 1 0%",
@@ -44,8 +55,7 @@ const ProjectsCard = () => {
               gap: "10px",
               alignContent: "center",
               padding: "calc(var(--content-pad) * .5) calc(var(--content-pad) * .5) calc(var(--content-pad) * .5) var(--content-pad)",
-            }}
-          >
+            }}>
             <div className="flex-c" style={{ flex: "1 1 0%" }}>
               <div style={{ display: "block", width: "100%" }}>
                 <form>
@@ -68,13 +78,15 @@ const ProjectsCard = () => {
                         />
                       </label>
                       <label className="flex-c Products__Label-ke3axf-0 ViAdX" style={{transition: "height 0.3s ease-in-out"}}>
+
+{/* Passed in the handleFocus and handleBlur functions to the onFocus and onBlur event handlers in the textarea */}
+
                         <textarea
                           maxLength="1500"
                           rows="1"
                           type="text"
-                          className={`form-control Products__InputTextArea-ke3axf-4 gAZTZj ${
-                            isTextareaFocused ? "textarea-focused" : ""
-                          }`}
+                       // Added in a ternary operator to add a class to the textarea in the event of "onFocus" in order to create conditional css styling.
+                          className={`form-control Products__InputTextArea-ke3axf-4 gAZTZj ${isTextareaFocused ? "textarea-focused" : ""}`}
                           placeholder="Description"
                           style={{ border: "none", width: "100%" }}
                           onFocus={handleFocus}
@@ -86,6 +98,7 @@ const ProjectsCard = () => {
                       <div className="d-flex justify-content-between">
                         <label className="flex-c w-50 Products__Label-ke3axf-0 ViAdX">
                           <span className="Products__Prefix-ke3axf-2 hMsDKK">$</span>
+
                           <input
                             type="text"
                             className="form-control Products__Input-ke3axf-3 JyYIe"
